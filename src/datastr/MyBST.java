f1427842532f9f57fcb2d3b28ed92736f0b46055
KarinaSkirmante <karina.krinkele@gmail.com>
2025-04-03 14:43:02 +0300

    Edit: added variables, isEmpty, isFull and howManyelements in MyBST

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -1,5 +1,36 @@
 package datastr;
 
-public class MyBST {
+public class MyBST<Ttype> {
 
+	private MyNode<Ttype> root = null;
+	private int counter = 0;
+	
+	//neveidošu beazrgumenta konstruktoru, jo tas ir jau no Object klases pieejams
+	
+	
+	public boolean isFull()//koks būs pilns, ja RAM atmiņā nebūs vietas 
+	{
+		try
+		{
+			MyNode<Integer> newNode = new MyNode<Integer>(4);
+			return false;
+			
+		}
+		catch (OutOfMemoryError e) {
+			return true;
+		}
+	}
+	
+	
+	public boolean isEmpty() {
+		return (counter == 0);
+	}
+
+	public int howManyElements() {
+		return counter;
+	}
+	
+	
+	
+	
 }
